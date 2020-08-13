9da4c19d16405c6214812b40c1d536a0d31c2611
Haseeb Zia <haseebzia99@outlook.com>
2020-08-13 17:55:06 +0100

    cocktail website

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,30 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(strong_params)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+  end
+end
+
+
+private
+
+def strong_params
+params.requie(:cocktail).permit(:name)
+end
+
+end
